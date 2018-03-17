386863f214d03c3183745cd68a7bd73601b6a961
take42038 <take42038@gmail.com>
2018-03-18 01:20:48 +0900

    iniファイルからセクション一覧の読み出し、セクションからキー一覧の読み出し処理作成

---
+++ DQXTarotApp/DQXTarotApp/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace DQXTarotApp
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbRank = new System.Windows.Forms.ComboBox();
+            this.lblRank = new System.Windows.Forms.Label();
+            this.lblMonster = new System.Windows.Forms.Label();
+            this.cmbMonster = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cmbRank
+            // 
+            this.cmbRank.Font = new System.Drawing.Font("MS UI Gothic", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cmbRank.FormattingEnabled = true;
+            this.cmbRank.Location = new System.Drawing.Point(359, 92);
+            this.cmbRank.Name = "cmbRank";
+            this.cmbRank.Size = new System.Drawing.Size(445, 45);
+            this.cmbRank.TabIndex = 0;
+            this.cmbRank.Text = "ーーーーーーー";
+            this.cmbRank.SelectionChangeCommitted += new System.EventHandler(this.cmbRank_SelectionChangeCommitted);
+            // 
+            // lblRank
+            // 
+            this.lblRank.AutoSize = true;
+            this.lblRank.Font = new System.Drawing.Font("MS UI Gothic", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblRank.Location = new System.Drawing.Point(114, 100);
+            this.lblRank.Name = "lblRank";
+            this.lblRank.Size = new System.Drawing.Size(171, 37);
+            this.lblRank.TabIndex = 1;
+            this.lblRank.Text = "ランク選択";
+            this.lblRank.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblMonster
+            // 
+            this.lblMonster.AutoSize = true;
+            this.lblMonster.Font = new System.Drawing.Font("MS UI Gothic", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblMonster.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblMonster.Location = new System.Drawing.Point(114, 208);
+            this.lblMonster.Name = "lblMonster";
+            this.lblMonster.Size = new System.Drawing.Size(231, 37);
+            this.lblMonster.TabIndex = 2;
+            this.lblMonster.Text = "モンスター選択";
+            // 
+            // cmbMonster
+            // 
+            this.cmbMonster.Font = new System.Drawing.Font("MS UI Gothic", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cmbMonster.FormattingEnabled = true;
+            this.cmbMonster.Location = new System.Drawing.Point(359, 200);
+            this.cmbMonster.Name = "cmbMonster";
+            this.cmbMonster.Size = new System.Drawing.Size(445, 45);
+            this.cmbMonster.TabIndex = 3;
+            this.cmbMonster.Text = "ランクを選択してください";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1174, 529);
+            this.Controls.Add(this.cmbMonster);
+            this.Controls.Add(this.lblMonster);
+            this.Controls.Add(this.lblRank);
+            this.Controls.Add(this.cmbRank);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "メイン画面";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbRank;
+        private System.Windows.Forms.Label lblRank;
+        private System.Windows.Forms.Label lblMonster;
+        private System.Windows.Forms.ComboBox cmbMonster;
+    }
+}
+
---
+++ DQXTarotApp/DQXTarotApp/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//using System;
+using System.IO;
+//using System.Text;
+using System.Runtime.InteropServices;
+
+
+namespace DQXTarotApp
+{
+    public partial class frmMain : Form
+    {
+        [DllImport("kernel32.dll")]
+        static extern int GetPrivateProfileSectionNames(
+            IntPtr lpszReturnBuffer,
+            uint nSize,
+            string lpFileName);
+
+        [DllImport("KERNEL32.DLL",EntryPoint = "GetPrivateProfileStringA")]
+        static extern int GetPrivateProfileStringByByteArray(
+            string lpAppName,
+            string lpKeyName, 
+            string lpDefault,
+            byte[] lpReturnedString, 
+            uint nSize,
+            string lpFileName);
+
+
+        [DllImport("KERNEL32.DLL")]
+        static extern int GetPrivateProfileString(
+            string lpAppName,
+            string lpKeyName,
+            string lpDefault,
+            StringBuilder lpReturnedString,
+            uint nSize,
+            string lpFileName);
+
+        public string path;
+        public string[] strArySection;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            cmbRank.Text = "ランクを選択してください。";
+
+            //iniファイルのパス設定
+            path = Application.StartupPath + Path.DirectorySeparatorChar + "Monster.ini";
+            
+            if (File.Exists(path))
+            {
+                // iniファイルよりセクション名一覧を取得
+                IntPtr ptr = Marshal.StringToHGlobalAnsi(new String('\0', 1024));
+                int length = GetPrivateProfileSectionNames(ptr, 1024, path);
+
+                //セクション名が取得できたら
+                if (0 < length)
+                {
+                    //セクション名一覧のポインタからstringへ
+                    String resultSection = Marshal.PtrToStringAnsi(ptr, length);
+
+                    //取得したセクション名一覧を配列へ退避
+                    strArySection = resultSection.Split( new[] { '\0' },StringSplitOptions.RemoveEmptyEntries);
+
+                    //取得したセクション名一覧をコンボボックスに設定
+                    Array.ForEach<String>(resultSection.Split(new[] { '\0' },StringSplitOptions.RemoveEmptyEntries), strRank => cmbRank.Items.Add(strRank));
+
+                    //コンボボックスの一番上に”すべて”を追加
+                    cmbRank.Items.Insert(0, "すべて");                    
+
+                }
+
+                Marshal.FreeHGlobal(ptr);
+
+
+
+            }
+        }
+
+        private void cmbRank_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            cmbMonster.Items.Clear();
+            cmbMonster.Text = "";
+
+            // iniファイルより指定したセクションのキー一覧を取得
+            //IntPtr ptr = Marshal.StringToHGlobalAnsi(new String('\0', 1024));
+            byte[] ar1 = new byte[1024];
+
+            //選択されたランクを設定
+            string strSelectRank = cmbRank.SelectedItem.ToString();
+
+            //選択されたランクが”すべて”の場合
+            if (strSelectRank == "すべて")
+            {
+                //退避していたセクション名一覧（全ランク）分のキー名一覧を取得
+                foreach (string s in strArySection)
+                {
+                    int lengthAll = GetPrivateProfileStringByByteArray(s, null, "", ar1, 1024, path);
+                    if (0 < lengthAll)
+                    {
+                        //取得したキー名一覧をコンボボックスに設定
+                        string resultKey = Encoding.Default.GetString(ar1, 0, lengthAll - 1);
+                        Array.ForEach<String>(resultKey.Split(new[] { '\0' }, StringSplitOptions.RemoveEmptyEntries), strMonster => cmbMonster.Items.Add(strMonster));
+                    }
+
+                }
+                cmbMonster.Text = "モンスターを選択してください。";
+
+            }
+            //選択されたランクが”すべて”以外の場合
+            else
+            {
+                //選択されたランクのセクションのキー名一覧を取得
+                int length = GetPrivateProfileStringByByteArray(strSelectRank, null, "", ar1, 1024, path);
+
+                //キーが取得できた場合
+                if (0 < length)
+                {
+                    //取得したキー名一覧をコンボボックスに設定
+                    string resultKey = Encoding.Default.GetString(ar1, 0, length - 1);
+                    Array.ForEach<String>(resultKey.Split(new[] { '\0' }, StringSplitOptions.RemoveEmptyEntries), strMonster => cmbMonster.Items.Add(strMonster));
+                    cmbMonster.Text = "モンスターを選択してください。";
+                }
+                //キーが取得できなかった場合
+                else
+                {
+                    cmbMonster.SelectedIndex = -1;
+                    cmbMonster.Items.Clear();
+                    cmbMonster.Items.Add("");
+                    cmbMonster.Text = "対象モンスターが見つかりません。";
+                }
+                
+            }
+
+        }
+
+
+    }
+}
